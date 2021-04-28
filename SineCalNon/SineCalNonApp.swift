6bfa778d93ee6053e557853e156f96b4ac2da56e
Andy Han <36119601+andyqhan@users.noreply.github.com>
2021-04-28 13:49:48 -0400

    initial commit

---
+++ SineCalNon/SineCalNonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SineCalNonApp.swift
+//  SineCalNon
+//
+//  Created by Andy Han on 4/28/21.
+//
+
+import SwiftUI
+
+@main
+struct SineCalNonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
